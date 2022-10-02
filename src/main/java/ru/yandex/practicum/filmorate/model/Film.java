aca4a86db7dc7e44a371e3caea06f0c1e956ab5c
dimavult <yandjava@gmail.com>
2022-10-02 23:58:24 +0300

    change duration field type Duration->long

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,21 +1,20 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Builder;
 import lombok.Data;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Data
-@Builder(toBuilder = true)
 public class Film {
     private int id;
     private String name;
     private String description;
     private LocalDate releaseDate;
-    private Duration duration;
+    private long duration;
 
-    public Film(int id, String name, String description, LocalDate releaseDate, Duration duration) {
+    public Film(int id, String name, String description, LocalDate releaseDate, long duration) {
         this.id = id;
         this.name = name;
         this.description = description;
@@ -23,7 +22,7 @@ public class Film {
         this.duration = duration;
     }
 
-    public Film(String name, String description, LocalDate releaseDate, Duration duration) {
+    public Film(String name, String description, LocalDate releaseDate, long duration) {
         this.name = name;
         this.description = description;
         this.releaseDate = releaseDate;
